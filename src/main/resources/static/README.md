ffd82f778a80c21011241a30ef88395c63e88302
leonelmontiel <leonel.montiel@outlook.com>
2022-10-26 23:07:14 -0300

    creando proyecto spring boot para hacer el login

---
+++ src/main/resources/static/README.md
@@ -0,0 +1,1 @@
+# DonarVida-PESCAR
---
+++ src/test/java/com/pescar/donarvida/DonarvidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pescar.donarvida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonarvidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
