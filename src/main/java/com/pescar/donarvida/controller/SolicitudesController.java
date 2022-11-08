2d64698fb99562a2b584e5da863958d2d9bc59e6
leonelmontiel <leonel.montiel@outlook.com>
2022-11-08 17:08:03 -0300

    levantamos proyecto spring

---
+++ src/main/java/com/pescar/donarvida/controller/SolicitudesController.java
@@ -0,0 +1,15 @@
+package com.pescar.donarvida.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class SolicitudesController {
+
+    @GetMapping("/solicitudes")
+	public String getSolicitudes(Model model) {
+		return "index";
+	}
+
+}
